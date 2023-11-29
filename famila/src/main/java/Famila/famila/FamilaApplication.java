3819419aa84e39e20dc939c877e9bc52b0c59905
OJiyeong <owldud215@gmail.com>
2023-11-29 17:47:02 +0900

    first commit

---
+++ famila/src/main/java/Famila/famila/FamilaApplication.java
@@ -0,0 +1,13 @@
+package Famila.famila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilaApplication.class, args);
+	}
+
+}
---
+++ famila/src/test/java/Famila/famila/FamilaApplicationTests.java
@@ -0,0 +1,13 @@
+package Famila.famila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
